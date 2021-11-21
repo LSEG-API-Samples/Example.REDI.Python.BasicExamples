f2cecea27969a641ba76bcb1727f4e66774114cc
Zoya Farberov <zfarberov@users.noreply.github.com>
2021-11-21 17:38:33 -0500

    Update l1.py
    added output

--- l1.py
+++ l1.py
@@ -6,17 +6,28 @@ from time import gmtime, strftime
 # Equity Order Entry Example
 q = win32com.client.Dispatch("REDI.Query")
  
-# Prepare a variable which can handle returned values from submit method of the order object.
+# Prepare a variable which can handle returned values.
 msg1 = VARIANT(pythoncom.VT_BYREF | pythoncom.VT_VARIANT, 0)
-msg2 = VARIANT(pythoncom.VT_BYREF | pythoncom.VT_VARIANT, 0)
-msg3 = VARIANT(pythoncom.VT_BYREF | pythoncom.VT_VARIANT, 0)
-msg4 = VARIANT(pythoncom.VT_BYREF | pythoncom.VT_VARIANT, 0)
+symbolVar = VARIANT(pythoncom.VT_BYREF | pythoncom.VT_VARIANT, 0)
+fieldVar = VARIANT(pythoncom.VT_BYREF | pythoncom.VT_VARIANT, 0)
+tgtVarName = VARIANT(pythoncom.VT_BYREF | pythoncom.VT_VARIANT, 0)
  
 vTable = "L1"
 vWhere = "true"
 tmpVal = q.Submit(vTable, vWhere, msg1)
  
-msg2.value = "IBM"
-msg3.value = "Last"
-q.GetL1Value(msg2, msg3, msg4)
-print(strftime("%Y-%m-%d %H:%M:%S", gmtime()) + " " + msg3.value + " " + msg4.value + " " + msg2.value)
+symbolVar.value = "IBM"
+ret = q.GetL1Value(symbolVar, "Last", tgtVarName)
+print(strftime("%Y-%m-%d %H:%M:%S", gmtime()) + " Symbol=" + tgtVarName.value + " Last" + "=" + str(symbolVar.value) + " success=" + str(ret))
+
+symbolVar.value = "T"
+ret =  q.GetL1Value(symbolVar, "Ask", tgtVarName)
+print(strftime("%Y-%m-%d %H:%M:%S", gmtime()) + " Symbol=" + tgtVarName.value + " " + "Ask" + "=" + str(symbolVar.value) + " success=" + str(ret))
+
+symbolVar.value = "BA"
+ret = q.GetL1Value(symbolVar, "Bid", tgtVarName)
+print(strftime("%Y-%m-%d %H:%M:%S", gmtime()) + " Symbol=" + tgtVarName.value + " " + "Bid" + "=" +  str(symbolVar.value) + " success=" + str(ret))
+
+symbolVar.value = "InvalidSymbol"
+ret = q.GetL1Value(symbolVar, "Bid", tgtVarName)
+print(strftime("%Y-%m-%d %H:%M:%S", gmtime()) + " Symbol=" + tgtVarName.value + " " + "InvalidSymbol" + "=" +  str(symbolVar.value) + " success=" + str(ret))
