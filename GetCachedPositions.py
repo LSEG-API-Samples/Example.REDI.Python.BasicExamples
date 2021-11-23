5ae042655775bd9e206cfffbf184f31e4b6ccab6
Zoya Farberov <zfarberov@users.noreply.github.com>
2021-11-22 22:17:04 -0500

    Draft version - in review (revised)
    Draft version - in review (revised)

--- GetCachedPositions.py
+++ GetCachedPositions.py
@@ -19,42 +19,41 @@ q = win32com.client.Dispatch("REDI.Query")
  
 # Prepare a variable which can handle returned values from submit method of the order object.
 row = VARIANT(pythoncom.VT_BYREF | pythoncom.VT_VARIANT, 0)
-column = VARIANT(pythoncom.VT_BYREF | pythoncom.VT_VARIANT, 0)
-cellValue = VARIANT(pythoncom.VT_BYREF | pythoncom.VT_VARIANT, 0)
-errCode = VARIANT(pythoncom.VT_BYREF | pythoncom.VT_VARIANT, 0)
+cellVar = VARIANT(pythoncom.VT_BYREF | pythoncom.VT_VARIANT, 0)
+cellVal = VARIANT(pythoncom.VT_BYREF | pythoncom.VT_VARIANT, 0)
+retVar = VARIANT(pythoncom.VT_BYREF | pythoncom.VT_VARIANT, 0)
 
 #add watch on accounts 
 myaccounts = ["EQUITY-TR"] 
 for account in myaccounts:
-	tmpVal = q.AddWatch("2", "", account, errCode);
-	print("account=" + account +  ", errCode=" + " " + str(errCode.value))
+	tmpVal = q.AddWatch("2", "", account, retVar);
+	print(strftime("%Y-%m-%d %H:%M:%S", gmtime()) + ":"+" account=" + account +  ", errCode=" + " " + str(retVar.value))
 
 # Prepare the query
 vTable = "Position"
 vWhere = "true"
-tmpVal = q.Submit(vTable, vWhere, errCode)
+tmpVal = q.Submit(vTable, vWhere, retVar)
 
 # Find out the number of available rows
 rowCount = q.RowCount
-print(strftime("%Y-%m-%d %H:%M:%S", gmtime()) + ": "+str(rowCount)+" rows present")
+print(strftime("%Y-%m-%d %H:%M:%S", gmtime()) + ": "+str(rowCount)+" rows (a.k.a. positions) exist")
  
 for i in range(0, rowCount):
 
-	row.value = i
-	column.value = "Account"
-	q.GetCell(row, column, cellValue , errCode)
-	print("row=" + str(row.value) + ", column="+column.value + ", cellValue=" + cellValue.value + ", errCode=" + " " + str(errCode.value))
+	cellVar.value = "Account"
+	ret = q.GetCell(i,  cellVar, cellVal, retVar)
+	print(str(retVar.value) + ", "+ str(cellVal.value) + "=" + str(cellVar.value) + " success="+str(ret))
 
-	column.value = "Symbol"
-	q.GetCell(row, column, cellValue , errCode)
-	print("row=" + str(row.value) + ", column="+column.value + ", cellValue=" + str(cellValue.value) + ", errCode=" + " " + str(errCode.value))
+	cellVar.value = "Symbol"
+	ret = q.GetCell(i,  cellVar, cellVal, retVar)
+	print(str(retVar.value) + ", "+ str(cellVal.value) + "=" + str(cellVar.value) + " success="+str(ret))
 
-	column.value = "Position"
-	q.GetCell(row, column, cellValue , errCode)
-	print("row=" + str(row.value) + ", column="+column.value + ", cellValue=" + str(cellValue.value) + ", errCode=" + " " + str(errCode.value))
+	cellVar.value = "Position"
+	ret = q.GetCell(i,  cellVar, cellVal, retVar)
+	print(str(retVar.value) + ", "+ str(cellVal.value) + "=" + str(cellVar.value) + " success="+str(ret))
 
-	column.value = "Value"
-	q.GetCell(row, column, cellValue , errCode)
-	print("row=" + str(row.value) + ", column="+column.value + ", cellValue=" + str(cellValue.value) + ", errCode=" + " " + str(errCode.value))
+	cellVar.value = "Value"
+	ret = q.GetCell(i,  cellVar, cellVal, retVar)
+	print(str(retVar.value) + ", "+ str(cellVal.value) + "=" + str(cellVar.value) + " success="+str(ret))
 
 	print("===>");
